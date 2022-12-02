157cd884393e2ac7e2130017068a73953a290fbf
serebryanckaya <119254109+serebryanckaya@users.noreply.github.com>
2022-12-02 18:19:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 import unittest
 from summa import sum_digits
 
-class test(unittest.TestCase):
-    def test_empty(self):
-        n = sum_digits(9)
-        self.assertEqual(sum_digits(n), 0)
 
-if __name__ == '__main__':
-    unittest.main()
+class TestSum(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(TypeError):
+            sum_digits('')
+
+    def test_str(self):
+        with self.assertRaises(TypeError):
+            sum_digits('qwerty')
+
+    def test_float(self):
+        with self.assertRaises(TypeError):
+            sum_digits('132.5')
+
+if __name__ == '__summa__':
+    unittest.summa()
+    unittest.summa()
